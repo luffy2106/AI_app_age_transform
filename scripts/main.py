97a28239fc64ed31bfab721324a5ed72995ae5dd
root <root@DESKTOP-NQDI2S4.localdomain>
2023-05-22 00:08:41 +0200

    Need to find out about template to build interface in fastAPI

---
+++ scripts/main.py
@@ -0,0 +1,167 @@
+from fastapi import FastAPI, status, UploadFile, File, Request
+import torch
+from loguru import logger
+import timeit
+import pprint
+from argparse import Namespace
+from models.psp import pSp
+import torchvision.transforms as transforms
+from PIL import Image
+import shutil
+import dlib
+from scripts.align_all_parallel import align_face
+from datasets.augmentations import AgeTransformer
+from fastapi.responses import HTMLResponse
+from utils.common import tensor2im
+from pathlib import Path
+from fastapi.responses import FileResponse
+from fastapi.templating import Jinja2Templates
+import os
+from fastapi.staticfiles import StaticFiles
+# Define Inference Parameters
+EXPERIMENT_DATA_ARGS = {
+        "model_path": "./pretrained_models/sam_ffhq_aging.pt",
+        "transform": transforms.Compose([
+            transforms.Resize((256, 256)),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+}
+
+
+
+
+
+
+def run_alignment(image_path, detect_face_config):
+    """
+    Detect face in the image
+    """
+    predictor = dlib.shape_predictor(detect_face_config)
+    aligned_image = align_face(filepath=image_path, predictor=predictor) 
+    logger.info("Aligned image has shape: {}".format(aligned_image.size))
+    return aligned_image
+
+def run_on_batch(inputs, net):
+    result_batch = net(inputs.to("cuda").float(), randomize_noise=False, resize=False)
+    return result_batch
+
+# Load model
+def load_model():
+    """
+    Load the model and move to GPU
+    """
+    model_path = EXPERIMENT_DATA_ARGS['model_path']
+    ckpt = torch.load(model_path, map_location='cpu')
+    opts = ckpt['opts']
+    # print object in a clear format
+    pprint.pprint(opts)
+    # Update the training options
+    opts['checkpoint_path'] = model_path
+    # Conver to name space object
+    opts = Namespace(**opts)
+    # Load model based on the architecture
+    net = pSp(opts)
+    # Set model to evaluation mode, disables these training-specific layers/operations. This ensures that the model produces consistent and deterministic outputs given the same input, which is desirable for making predictions.
+    net.eval()
+    # Move model from CPU to GPU
+    net.cuda()
+    logger.success('Model successfully loaded!')
+
+
+# Set up format for log
+logger.add(
+    sink="log_file.log",
+    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level}</level> | <cyan>{message}</cyan>"
+)
+
+
+# @app.get("/", response_class=HTMLResponse)
+# async def root():
+#     return """
+#         <html>
+#             <head>
+#                 <title>Upload a File and choose an age</title>
+#             </head>
+#             <body>
+#                 <form action="/submit" method="post" enctype="multipart/form-data">
+#                     <label for="age">Fill in an age you want to see:</label>
+#                     <input type="age" id="age" name="age" min="1" max="90">
+#                     <br>
+#                     <label for="file">Upload a file:</label>
+#                     <input type="file" id="file" name="file">
+#                     <br>
+#                     <button type="submit">Submit</button>
+#                 </form>
+#             </body>
+#         </html>
+#     """
+# Define application
+app = FastAPI()
+#app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def upload(request: Request):
+   """
+   Create template to upload image
+   """
+   logger.info("start uploading")
+   current_dir = os.getcwd()
+   logger.info("Current Working Directory: ", current_dir)
+   return templates.TemplateResponse("uploadfile.html", {"request": request})
+
+
+
+# Define the API route
+@app.post(
+    "/uploader/",
+    summary="Process an image",
+    status_code=status.HTTP_200_OK,
+    response_class=HTMLResponse
+)
+async def predict(
+    age: int,
+    file: UploadFile = File(...)
+):
+    """
+    Predicts a class of an image
+    """
+    starttime = timeit.default_timer()
+    logger.info(f"Image inference start")
+    starttime = timeit.default_timer()
+    model = load_model()
+
+    # Upload image and save it
+    with open("tmp.png", "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    # Set up input
+    image_path = "tmp.jpg"
+    # Align image
+    detect_face_config = "shape_predictor_68_face_landmarks.dat"
+    aligned_image = run_alignment(image_path, detect_face_config)
+
+    # Run inference 
+    img_transforms = EXPERIMENT_DATA_ARGS['transform']
+    input_image = img_transforms(aligned_image)
+    age_transformers = AgeTransformer(target_age=int(age))
+
+    logger.info(f"Running on target age: {age_transformers.target_age}")
+    with torch.no_grad():
+        input_image_age = [age_transformers(input_image.cpu()).to('cuda')]
+        input_image_age = torch.stack(input_image_age)
+        result_tensor = run_on_batch(input_image_age, model)[0]
+        result_image = tensor2im(result_tensor)
+        # results = np.concatenate([results, result_image], axis=1)
+        # results = Image.fromarray(result_image)
+        result_image.save("age_transform.jpg")
+
+    logger.info(f"Time of inference {timeit.default_timer() - starttime}")
+
+    return {"filename": file.filename}
+
+@app.get("/show_results")
+async def main():
+    img_path = Path("age_transform.jpg")
+    if img_path.is_file():
+        return FileResponse("age_transform.jpg")
